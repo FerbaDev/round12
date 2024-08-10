853df09e1b00a81c52fc829ea5b13a67910d95ab
FerbaDev <elferbarron1@gmail.com>
2024-08-10 09:46:22 -0300

    se trabaja en carrusel

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-const logo = document.querySelector("#logo");
+// const nav = document.querySelector("#nav");
+// const abrir = document.querySelector("#abrir");
+// const cerrar = document.querySelector("#cerrar");
+// const logo = document.querySelector("#logo");
 
-abrir.addEventListener("click", () => {
-  nav.classList.add("visible");
+// abrir.addEventListener("click", () => {
+//   nav.classList.add("visible");
+// });
+
+// cerrar.addEventListener("click", () => {
+//   nav.classList.remove("visible");
+// });
+
+// logo.addEventListener("click", () => {
+//   //enlace de la pagina que quieras
+//   window.location.href = "./index.html";
+// });
+
+
+//carrusel
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-control.next');
+const prevButton = document.querySelector('.carousel-control.prev');
+let currentIndex = 0;
+
+const updateCarousel = () => {
+    // Elimina las clases activas para todos los slides
+    slides.forEach(slide => slide.style.order = '');
+
+    // Asigna el orden en el cual deben aparecer las imágenes
+    for (let i = 0; i < slides.length; i++) {
+        let index = (currentIndex + i) % slides.length;
+        slides[i].style.order = index;
+    }
+};
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
 });
 
-cerrar.addEventListener("click", () => {
-  nav.classList.remove("visible");
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
 });
 
-logo.addEventListener("click", () => {
-  //enlace de la pagina que quieras
-  window.location.href = "./index.html";
-});
+// Inicializa el carrusel
+updateCarousel();
