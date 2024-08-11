baf75ba330a3658e0849639e2da3e1d4f270e76f
FerbaDev <elferbarron1@gmail.com>
2024-08-11 19:49:23 -0300

    se trabaja en hero mobile

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-// const nav = document.querySelector("#nav");
-// const abrir = document.querySelector("#abrir");
-// const cerrar = document.querySelector("#cerrar");
-// const logo = document.querySelector("#logo");
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const logo = document.querySelector("#logo");
 
-// abrir.addEventListener("click", () => {
-//   nav.classList.add("visible");
-// });
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
 
-// cerrar.addEventListener("click", () => {
-//   nav.classList.remove("visible");
-// });
+cerrar.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
 
 // logo.addEventListener("click", () => {
 //   //enlace de la pagina que quieras
